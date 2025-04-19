3ae63805356e175dd62890068683b4003bc1a697
GN0MI0 <103611243+SauloJose@users.noreply.github.com>
2025-04-18 22:43:44 -0300

    Update app.js

--- MeteoPIN-main/public/src/js/app.js
+++ MeteoPIN-main/public/src/js/app.js
@@ -4,13 +4,13 @@ import {getDatabase} from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-da
 
 //Dado EXTREMAMENTE SENSÍVEL!
 const firebaseConfig = {
-  apiKey: "AIzaSyByoag6JqVq68CLEMU5DrlX9KIHF6DNjWM",
-  authDomain: "meteopin-7b28c.firebaseapp.com",
-  databaseURL: "https://meteopin-7b28c-default-rtdb.firebaseio.com",
-  projectId: "meteopin-7b28c",
-  storageBucket: "meteopin-7b28c.appspot.com",
-  messagingSenderId: "60683217267",
-  appId: "1:60683217267:web:78eafc68a6beb79acc7d06"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId:"" ,
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Inicializando a base de dados.
